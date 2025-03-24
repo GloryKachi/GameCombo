02bf2ec077917b25f2561f22417143a5896ba774
GloryKachi <kachikachina@gmail.com>
2025-03-24 13:08:07 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Tetris from "@/components/tetris"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <Tetris />
+    </main>
+  )
+}
+
